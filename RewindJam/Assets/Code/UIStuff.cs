3077a43aa7c647bd95d57f6dbeed29e4046cf0ff
grommithub <44238640+grommithub@users.noreply.github.com>
2020-08-02 17:49:54 +0200

    World's worst menu

---
+++ RewindJam/Assets/Code/UIStuff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStuff : MonoBehaviour
+{
+    private Vector3 _scale;
+    [SerializeField] private float _tweenTime = 0.3f;
+    private void OnEnable()
+    {
+        _scale = transform.localScale;
+        transform.localScale = Vector3.zero;
+        LeanTween.scale(gameObject, _scale, _tweenTime);
+    }
+}
