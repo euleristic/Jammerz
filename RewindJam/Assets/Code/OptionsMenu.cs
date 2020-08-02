990a8cd3c0dc4f48dc9f902136ae58fca2ea57ae
GaggyV <60134108+GaggyV@users.noreply.github.com>
2020-08-02 22:05:01 +0200

    Audio options now work xD

---
+++ RewindJam/Assets/Code/OptionsMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider masterSlider, musicSlider, sfxSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+
+    public void SetMasterVol()
+    {
+       mixer.SetFloat("MasterVolume", masterSlider.value);
+    }
+
+    public void SetMusicVol()
+    {
+        mixer.SetFloat("MusicVolume", masterSlider.value);
+    }
+
+    public void SetSFXvol()
+    {
+        mixer.SetFloat("SFXvolume", masterSlider.value);
+    }
+}
